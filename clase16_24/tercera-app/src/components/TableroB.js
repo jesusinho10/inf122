80b9f68f578c1bca46adee7a24957758299b9ad6
jesusinho10 <155926529+jesusinho10@users.noreply.github.com>
2024-01-16 14:05:43 +0000

    terzera

---
+++ clase16_24/tercera-app/src/components/TableroB.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import '../style/Tablero.css';
+import CuadroB from './CuadroB';
+
+function TableroB() {
+    const click = () => {
+        if (valor === "X") {
+            setValor("O");
+        } else {
+            setValor("X");
+        }
+        
+        console.log('click');
+    }
+    return (
+        <div className="tablero">
+            <CuadroB valor={0} funcion={click} />
+            <CuadroB valor={1} funcion={click} />
+            <CuadroB valor={2} funcion={click} />
+            <CuadroB valor={3} funcion={click} />
+            <CuadroB valor={4} funcion={click} />
+            <CuadroB valor={5} funcion={click} />
+            <CuadroB valor={6} funcion={click} />
+            <CuadroB valor={7} funcion={click} />
+            <CuadroB valor={8} funcion={click} />
+        </div>
+
+    );
+}
+export default TableroB;
